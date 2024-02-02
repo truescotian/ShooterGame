48a43facf3cd3283aef370fe6af8113d88d8452e
Greg Miller <gregpmillr@gmail.com>
2024-02-01 20:49:41 -0400

    Added UI and transitions between scenes

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -3,20 +3,22 @@ class_name LevelParent
 
 var laser_scene: PackedScene = preload("res://scenes/projectiles/laser.tscn")
 var grenade_scene: PackedScene = preload("res://scenes/projectiles/grenade.tscn")
-var steamvent_scene: PackedScene = preload("res://scenes/objects/steam_vent.tscn")
 
+	
 func _on_player_laser(pos, direction):
 	var laser = laser_scene.instantiate() as Area2D
 	laser.position = pos
 	laser.rotation_degrees = rad_to_deg(direction.angle()) + 90 
 	laser.direction = direction
 	$Projectiles.add_child(laser)
+	$UI.update_laser_text()
 
 func _on_player_grenade(pos, direction):
 	var grenade = grenade_scene.instantiate() as RigidBody2D
 	grenade.position = pos
 	grenade.linear_velocity = direction * grenade.speed
 	$Projectiles.add_child(grenade)
+	$UI.update_grenade_text()
 
 
 func _on_house_player_entered():
--- scenes/levels/player.gd
+++ scenes/levels/player.gd
@@ -21,7 +21,8 @@ func _process(_delta):
 	
 	# laser shooting input
 	var player_direction = (get_global_mouse_position() - position).normalized()			
-	if Input.is_action_pressed("primary action") and can_laser:
+	if Input.is_action_pressed("primary action") and can_laser and Globals.laser_amount > 0:
+		Globals.laser_amount -= 1
 		$GPUParticles2D.emitting = true		
 		var laser_markers = $LaserStartPositions.get_children()
 		var selected_laser = laser_markers[randi() % laser_markers.size()]
@@ -30,7 +31,8 @@ func _process(_delta):
 		var pos = selected_laser.global_position
 		laser.emit(pos, player_direction)
 
-	if Input.is_action_pressed("secondary action") and can_grenade:
+	if Input.is_action_pressed("secondary action") and can_grenade and Globals.grenade_amount > 0:
+		Globals.grenade_amount -= 1
 		can_grenade = false
 		$GrenadeReloadTimer.start()
 		var pos = $LaserStartPositions.get_children()[2].global_position
