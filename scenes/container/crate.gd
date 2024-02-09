a409a0d33c3a4874df39cc571bc3db0531dbfdd4
Greg Miller <gregpmillr@gmail.com>
2024-02-09 18:34:22 -0400

    Crate, toilet with powerups. Some grime on the gloor inside

--- scenes/container/crate.gd
+++ scenes/container/crate.gd
@@ -1,4 +1,9 @@
 extends ItemContainer
 
 func hit():
-	print("hit crate!")
+	if not opened:
+		$LidSprite.hide()
+		for i in range(5):
+			var pos = $SpawnPositions.get_child(randi()%$SpawnPositions.get_child_count()).global_position
+			open.emit(pos, current_direction)
+		opened = true
--- scenes/container/item_container.gd
+++ scenes/container/item_container.gd
@@ -1,5 +1,6 @@
 extends StaticBody2D
 class_name ItemContainer
 
-func hit():
-	print('object')
+@onready var current_direction: Vector2 = Vector2.DOWN.rotated(rotation)
+var opened: bool = false
+signal open(pos, direction)
